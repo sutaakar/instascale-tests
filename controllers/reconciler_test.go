61a1d8d73fc2dfbfdc353be72836701088e29523
Karel Suta <ksuta@redhat.com>
2024-01-08 21:14:57 +0100

    test MachineSets

--- controllers/reconciler_test.go
+++ controllers/reconciler_test.go
@@ -3,12 +3,17 @@ package controllers
 import (
 	"context"
 	"go/build"
+	"os"
 	"path/filepath"
 	"testing"
 	"time"
 
 	. "github.com/onsi/gomega"
+	gstruct "github.com/onsi/gomega/gstruct"
+	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
+	machinev1 "github.com/openshift/client-go/machine/clientset/versioned"
 	. "github.com/project-codeflare/codeflare-common/support"
+	"github.com/project-codeflare/instascale/pkg/config"
 	mcadv1beta1 "github.com/project-codeflare/multi-cluster-app-dispatcher/pkg/apis/controller/v1beta1"
 	mc "github.com/project-codeflare/multi-cluster-app-dispatcher/pkg/client/clientset/versioned"
 	apiv1 "k8s.io/api/core/v1"
@@ -38,6 +43,7 @@ func startEnvTest(t *testing.T) *envtest.Environment {
 		CRDDirectoryPaths: []string{
 			filepath.Join("..", "config", "crd", "bases"),
 			filepath.Join(build.Default.GOROOT, "pkg", "mod", "github.com", "project-codeflare", "multi-cluster-app-dispatcher@v1.38.1", "config", "crd", "bases"),
+			filepath.Join(build.Default.GOROOT, "pkg", "mod", "github.com", "openshift", "api@v0.0.0-20220411210816-c3bb724c282a", "machine", "v1beta1"),
 		},
 	}
 	cfg, err = testEnv.Start()
@@ -51,6 +57,7 @@ func startEnvTest(t *testing.T) *envtest.Environment {
 func establishClient(t *testing.T) {
 	test := With(t)
 	err = mcadv1beta1.AddToScheme(scheme.Scheme)
+	err = machinev1beta1.AddToScheme(scheme.Scheme)
 	test.Expect(err).NotTo(HaveOccurred())
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
@@ -65,7 +72,10 @@ func establishClient(t *testing.T) {
 	instaScaleController := &AppWrapperReconciler{
 		Client: k8sManager.GetClient(),
 		Scheme: k8sManager.GetScheme(),
-		// Config: cfg.InstaScale.InstaScaleConfiguration,
+		Config: config.InstaScaleConfiguration{
+			MachineSetsStrategy: "reuse",
+			MaxScaleoutAllowed:  5,
+		},
 	}
 	err = instaScaleController.SetupWithManager(context.Background(), k8sManager)
 	test.Expect(err).ToNot(HaveOccurred())
@@ -89,11 +99,25 @@ func TestReconciler(t *testing.T) {
 	testEnv = startEnvTest(t)
 	defer teardownTestEnv(testEnv)
 
+	machineClient, err := machinev1.NewForConfig(cfg)
+	With(t).Expect(err).ToNot(HaveOccurred())
+
+	b, err := os.ReadFile("qqq.txt")
+	With(t).Expect(err).ToNot(HaveOccurred())
+
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	ms, _, err := decode(b, nil, nil)
+	With(t).Expect(err).ToNot(HaveOccurred())
+	msa := ms.(*machinev1beta1.MachineSet)
+
+	ms, err = machineClient.MachineV1beta1().MachineSets("default").Create(With(t).Ctx(), msa, metav1.CreateOptions{})
+	With(t).Expect(err).ToNot(HaveOccurred())
+
 	app := &mcadv1beta1.AppWrapper{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "mnist",
 			Labels: map[string]string{
-				"orderedinstance": "test.instance1_test.instance2",
+				"orderedinstance": "test.instance1",
 			},
 		},
 		Spec: mcadv1beta1.AppWrapperSpec{
@@ -126,6 +150,9 @@ func TestReconciler(t *testing.T) {
 	_, err = mcadClient.WorkloadV1beta1().AppWrappers("default").Create(With(t).Ctx(), app, metav1.CreateOptions{})
 	With(t).Expect(err).ToNot(HaveOccurred())
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(10 * time.Second)
 
+	machineset, err := machineClient.MachineV1beta1().MachineSets("default").Get(With(t).Ctx(), "test-instascale", metav1.GetOptions{})
+	With(t).Expect(err).ToNot(HaveOccurred())
+	With(t).Expect(machineset.Spec.Replicas).To(gstruct.PointTo(Equal(int32(1))))
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/onsi/gomega v1.27.10
 	github.com/openshift-online/ocm-sdk-go v0.1.368
 	github.com/openshift/api v0.0.0-20230213134911-7ba313770556
+	github.com/openshift/client-go v0.0.0-20221019143426-16aed247da5c
 	github.com/project-codeflare/codeflare-common v0.0.0-20231213113014-78776a40b4a8
 	github.com/project-codeflare/multi-cluster-app-dispatcher v1.38.1
 	k8s.io/api v0.26.3
@@ -50,7 +51,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/openshift/client-go v0.0.0-20221019143426-16aed247da5c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
